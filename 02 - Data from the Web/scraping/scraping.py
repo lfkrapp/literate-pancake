4bbcf8a4bf866e871a5406114008c2fe9a4d5c07
lfkrapp <lucien.krapp@epfl.ch>
2016-10-11 11:16:43 +0200

    scraping.py working without bugs, set download path and url and launch script

--- 02 - Data from the Web/scraping/scraping.py
+++ 02 - Data from the Web/scraping/scraping.py
@@ -4,25 +4,14 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select, WebDriverWait
 
+# element_names = ['ww_i_reportModelXsl', 'zz_x_UNITE_ACAD0', 'ww_x_UNITE_ACAD', 'zz_x_PERIODE_ACAD', 'ww_x_PERIODE_ACAD', 'zz_x_PERIODE_PEDAGO', 'ww_x_PERIODE_PEDAGO', 'zz_x_HIVERETE', 'ww_x_HIVERETE']
 
-def print_elements_by_name(driver):
-	ids = driver.find_elements_by_xpath('//*[@name]')
-	for ii in ids:
-    		print ii.get_attribute('name')    # id name as string
-
-# names = ['ww_i_reportModelXsl', 'zz_x_UNITE_ACAD0', 'ww_x_UNITE_ACAD', 'zz_x_PERIODE_ACAD', 'ww_x_PERIODE_ACAD', 'zz_x_PERIODE_PEDAGO', 'ww_x_PERIODE_PEDAGO', 'zz_x_HIVERETE', 'ww_x_HIVERETE']
-
-# !!! DOWNLOAD FOLDER MUST BE EMPTY !!!
-download_path = "/home/khaos/Downloads"
+# ------- SETUP VARIABLES -------
+download_path = "/home/khaos/Downloads" # !!! DOWNLOAD FOLDER MUST BE EMPTY !!!
 url = 'http://isa.epfl.ch/imoniteur_ISAP/%21gedpublicreports.htm?ww_i_reportmodel=133685247'
 
 # ------- INITIALIZATION -------
 profile = webdriver.FirefoxProfile()
-# profile.set_preference('browser.download.folderList', 2)
-# profile.set_preference('browser.download.manager.showWhenStarting', False)
-# profile.set_preference('browser.download.dir', os.getcwd())
-# profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'XLS')
-# driver = webdriver.Firefox(profile)
 profile.set_preference("browser.download.panel.shown", False)
 profile.set_preference("browser.helperApps.neverAsk.openFile","text/csv,application/vnd.ms-excel")
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/vnd.ms-excel")
@@ -37,6 +26,7 @@ driver.switch_to_frame(driver.find_element_by_name('toc'))
 
 # print_elements_by_name(driver)
 
+# ------- SCRAPING -------
 print('Start scraping website...')
 
 radiobtn = driver.find_element_by_xpath("//input[@name='ww_i_reportModelXsl'][@value='133685271']")
@@ -45,7 +35,6 @@ radiobtn.click()
 okbtn = driver.find_element_by_name('dummy')
 okbtn.click()
 
-# test = driver.find_element_by_class_name('ww_x_GPS')
 # n = 10
 n = 7613+2
 for i in range(2,n):
@@ -58,10 +47,10 @@ for i in range(2,n):
         print(test.text)
         fversion = str(len(glob.glob(download_path+'/'+"".join(test.text.split())+"*")))
         os.rename(download_path+'/!GEDPUBLICREPORTS.XLS', download_path+"/"+"".join(test.text.split())+"_"+fversion+'.XLS')
-        while glob.glob(download_path+'/'+"".join(test.text.split())):
+        while len(glob.glob(download_path+'/'+"".join(test.text.split())+"_"+fversion+'.XLS'))==0:
             sleep(0.05)
 
-# ------ LOOP METHOD -------
+# ------ LOOP METHOD ------- (NOT IMPLEMENTED)
 # loop over an element
 # select = driver.find_element_by_name('ww_x_PERIODE_ACAD')
 # options = select.find_elements_by_tag_name('option')
@@ -77,5 +66,5 @@ for i in range(2,n):
 
 
 
-print('Script ended')
-# driver.quit()
+print('Scraping complete, closing browser...')
+driver.quit()
