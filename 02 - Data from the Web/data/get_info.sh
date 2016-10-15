3829691ccd61382aee5a29942e12849bf6d2ffa3
pierre <pierre@pierre>
2016-10-15 15:44:47 +0200

    Info data

---
+++ 02 - Data from the Web/data/get_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir info
+cp pit/Informatique* info
+rm info/Informatiquedebase*
+rm info/Informatiquetech*
+cd info 
+mkdir bachelor master
+mv *Bachelor bachelor/
+mv *Master master/
+
+
+
+
